05d1de124bc3e918c99ee4645f021bbfa64fcc4f
Vinicius Ruoso <vkr07@c3sl.ufpr.br>
2008-08-22 09:38:55 -0300

    sdiweb: Updated generatesdibar to include enabled summaries
    The sdi bar should have all summaries on page selection box. To do this
    the SNAME variable is used, and the target of page is the same name as the
    summary configuration file name.
    
    Signed-off-by: Vinicius Ruoso <vkr07@c3sl.ufpr.br>
    Signed-off-by: Bruno Ribas <ribas@c3sl.ufpr.br>

--- sdiweb/generatesdibar.sh
+++ sdiweb/generatesdibar.sh
@@ -3,9 +3,7 @@ OPTIONS=""
 
 function generatesdibar()
 {
-    if [ "$GENERATESUMMARY" == "true" ]; then
-        add_option sumary SUMARIO
-    fi
+    add_summaries
 
     for TYPE in $CLASSESDIR/*; do
         add_option single "$CLASSNAME/$(basename $TYPE)"
@@ -14,11 +12,20 @@ function generatesdibar()
     sed "s#(PAGES)#${OPTIONS}#g" $BARHTML
 }
 
+function add_summaries()
+{
+    for SUMMARY in $(\ls $PREFIX/summaries-enabled/*); do
+        source $SUMMARY
+        getsummaryinfo
+        add_option summary $SNAME $(basename $(realpath $SUMMARY))
+        unset SNAME
+    done
+}
+
 function add_option()
 {
-    if [ $1 == "sumary" ]; then
-        LOWER=$(echo $2 |tr '[:upper:]' '[:lower:]')
-        OPTIONS="${OPTIONS}    <option value=\"${LOWER}.html\">$2</option>\n"
+    if [ $1 == "summary" ]; then
+        OPTIONS="${OPTIONS}    <option value=\"$3.html\">$2</option>\n"
     else
         NAME=$(basename $2)
         SHOW=$(echo $2 |tr '_' ' ')
