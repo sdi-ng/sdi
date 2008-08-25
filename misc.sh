d2594814aea8bcc543406cd1db6771c8425c3f18
Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>
2008-08-25 15:42:49 -0300

    New file misc.sh. This file provides miscellaneous functions
    Functions shared by several scripts will be placed on misc.sh file. For now
    it provides a function to save a message on sdi log file.
    
    Signed-off-by: Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>
    Signed-off-by: Bruno Ribas <ribas@c3sl.ufpr.br>

---
+++ misc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+PREFIX=.
+
+source $PREFIX/sdi.conf
+
+: ${LOG:=$PREFIX/sdi.log}
+
+# Function to update the sdi log.
+# The log message contains the seconds since
+# 1970 and the contents of $1 parameter
+function LOG()
+{
+    echo "$(date +%s) $1" >> $LOG
+}
+
