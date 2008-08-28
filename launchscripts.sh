b36ea298d28ce541d491bcd69834c3d1d5fa7e59
Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>
2008-08-28 10:15:33 -0300

    Fix launchscripts.sh to check if vital files exists
    To avoid massive cron error e-mails, launchscripts.sh check whether vital
    files does exists, such as misc.sh AND more important cron requested HOOK.
    
    Signed-off-by: Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>
    Signed-off-by: Bruno Ribas <ribas@c3sl.ufpr.br>

--- launchscripts.sh
+++ launchscripts.sh
@@ -2,11 +2,24 @@
 
 sdiroot=.
 
-source $sdiroot/sdi.conf
+if ! source $sdiroot/misc.sh; then
+    echo "SDI(cron): failed to load $sdiroot/misc.sh"
+    exit 1
+fi
+
+if ! source $sdiroot/sdi.conf; then
+    LOG "CRON: failed to load sdi configuration file: $sdiroot/sdi.conf"
+    exit 1
+fi
 
 : ${CMDDIR:=$sdiroot/cmds}
 : ${HOOKS:=$sdiroot/commands-enabled}
 
 SOURCE="$1"
 
-cat $HOOKS/${SOURCE}.d/* >> $CMDDIR/general
+if ! test -f $CMDDIR/general; then
+    LOG "CRON: file not found: $CMDDIR/general"
+elif test -d $HOOKS/$SOURCE.d; then
+    cat $HOOKS/$SOURCE.d/* >> $CMDDIR/general
+fi
+
