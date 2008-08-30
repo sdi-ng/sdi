5076e3b7ef1edc5dd71d390728a52714a9cc07c7
Vinicius Ruoso <vkr07@c3sl.ufpr.br>
2008-08-30 20:23:01 -0300

    Created launchsendfile.sh to administer file transfer
    This script simply manage a queue and send information to host when the
    transfer ends. The variable SENDLIMIT has been added to control the number
    of transfers at the same time.
    
    Signed-off-by: Vinicius Ruoso <vkr07@c3sl.ufpr.br>
    Signed-off-by: Bruno Ribas <ribas@c3sl.ufpr.br>

---
+++ launchsendfile.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+PREFIX=$(dirname $0)
+
+source $PREFIX/sdi.conf
+
+# Customizable variables, please refer to sdi.conf to change these values
+: ${SENDLIMIT:=1}
+: ${TMPDIR:=/tmp/sdi}
+: ${PIDDIR:=$TMPDIR/pids}
+
+# the sendfile fifo
+FILEFIFO="$TMPDIR/sendfilefifo"
+FINISH="/tmp/.sdi.sendfile.finish"
+
+# create the pids folder
+mkdir -p $PIDDIR/sendfile
+
+# create the fifo itself
+rm -f $FILEFIFO 2> /dev/null
+mkfifo $FILEFIFO
+
+# function to wait a scp ends
+# adictionaly removes PID from pids file
+function waittransferend()
+{
+    PID=$1
+    DESTINATION=$2
+    while ps --pid $PID &> /dev/null; do
+        sleep 0.5
+    done
+    sed -i "/^$PID$/d" $PIDDIR/sendfile/transfers.pid
+    echo 'echo $(date +%s) '$DESTINATION' >> '$FINISH'' >> $CMDDIR/$HOST
+}
+
+
+# this function will listen to the fifo and
+# will launch the files transfers
+function sendfiledeamon()
+{
+    (tail -f $FILEFIFO & echo $! > $PIDDIR/sendfile/tailfifo.pid) |
+    while read HOST FILE DESTINATION LIMIT; do
+        # wait to send file
+        RUNNING=$(cat $PIDDIR/sendfile/transfers.pid |wc -l)
+        while (( $RUNNING >= $SENDLIMIT )); do
+            sleep 10
+            RUNNING=$(cat $PIDDIR/sendfile/transfers.pid |wc -l)
+        done
+
+        # check limit
+        if (( $LIMIT == 0 )); then
+            LIMIT=""
+        else
+            LIMIT="-l$LIMIT"
+        fi
+
+        # run scp
+        scp $LIMIT $FILE $SDIUSER@$HOST:$DESTINATION &
+
+        # send a waittransferend look to this proccess
+        PID=$!
+        echo $PID >> $PIDDIR/sendfile/transfers.pid
+        waittransferend $PID $DESTINATION &
+    done
+}
+
+# run the deamon
+sendfiledeamon &
+echo $! > $PIDDIR/sendfile/deamon.pid
