50a50c17b7918b841ff16941d0d49f0ba02702f8
Vinicius Ruoso <vkr07@c3sl.ufpr.br>
2009-01-20 14:07:01 -0200

    Fix SDITUNNEL() to correctly work when ssh fails
    We were using tail with --pid=$SDITUNNELPID, but when ssh fails the tail
    still there, stacking the function until SDI receive a close command.
    
    With this tail on background the script works normally, but it's necessary
    to kill the tail after ssh exits.
    
    This commit fix a regression added by commit 916fd6ef43c.
    
    Signed-off-by: Vinicius Ruoso <vkr07@c3sl.ufpr.br>
    Signed-off-by: Bruno Ribas <ribas@c3sl.ufpr.br>

--- launchsditunnel.sh
+++ launchsditunnel.sh
@@ -201,10 +201,11 @@ function SDITUNNEL()
         touch $CMDFILE
         printf "$HOST STATUS+OFFLINE\n" > $PARSERCOM
         (cat $HOOKS/onconnect.d/* 2>/dev/null;
-        tail -fq -n0 --pid=$SELF $CMDFILE $CMDGENERAL) |
+        tail -fq -n0 $CMDFILE $CMDGENERAL & echo $! > $PIDDIRHOSTS/$HOST.tail)|
             ssh $SSHOPTS -l $SDIUSER $HOST "bash -s" 2>&1 |
                 xargs -d'\n' -L 1 echo $HOST > $PARSERCOM
         printf "$HOST STATUS+OFFLINE\n" > $PARSERCOM
+        kill $(cat $PIDDIRHOSTS/$HOST.tail) && rm -f $PIDDIRHOSTS/$HOST.tail
         (test -f $TMPDIR/SDIFINISH || test -f $TMPDIR/${HOST}_FINISH) && break
         sleep $(bc <<< "($RANDOM%600)+120")
     done
