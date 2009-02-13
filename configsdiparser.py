6e3f9c2185251461b21cde7affe5f804d9d55f20
Vinicius Ruoso <vkr07@c3sl.ufpr.br>
2009-02-13 15:25:38 -0200

    Created a parser to SDI configuration file
    When running SDI, some important variables must be defined. At this time,
    the sdi.conf file are sourced by scripts that need those variables.
    
    This config parser will be responsable to get all informations on sdi.conf
    file, on a better way than source command.
    
    The sdi.conf will be separated on 4 sections:
      - general: some main information to SDI
      - ssh: ssh information to connect with hosts
      - web: definition of web related info
      - send file: sendfile feature config
    
    Signed-off-by: Vinicius Ruoso <vkr07@c3sl.ufpr.br>

---
+++ configsdiparser.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import sys
+
+# check python version to import configparser
+if sys.version_info[0]<3:
+    from ConfigParser import ConfigParser as configparser
+else:
+    from configparser import configparser
+
+class configsdiparser:
+
+    def __init__(self, conf='sdi.conf'):
+        # define default variable values
+        self.defaults = {
+            'general':   {'prefix': '.',
+                          'cmd dir': '%(prefix)s/cmds',
+                          'cmd general': '%(cmd dir)s/general',
+                          'data dir': '%(prefix)s/data',
+                          'tmp dir': '/tmp/sdi',
+                          'pid dir': '%(tmp dir)s/pids',
+                          'pid dir sys': '%(pid dir)s/system',
+                          'pid dir hosts': '%(pid dir)s/hosts',
+                          'shooks': '%(prefix)s/states-enabled',
+                          'launch delay': '0.05',
+                          'kill tout': '30',
+                          'log': '%(prefix)s/sdi.log',
+                          'hooks':  '%(prefix)s/commands-enabled',
+                          'fifo dir':  '%(tmp dir)s/fifos',
+                          'sfifo': '%(fifo dir)s/states.fifo'},
+            'ssh':       {'sdiuser': 'root',
+                          'timeout': '240',
+                          'sshopt[0]': 'PreferredAuthentications=publickey',
+                          'sshopt[1]': 'StrictHostKeychecking=no',
+                          'sshopt[2]': 'ConnectTimeout=%(timeout)s',
+                          'sshopt[3]': 'TCPKeepAlive=yes',
+                          'sshopt[4]': 'ServerAliveCountMax=3',
+                          'sshopt[5]': 'ServerAliveInterval=100'},
+            'web':       {'web mode': 'true',
+                          'sdi web': 'sdiweb',
+                          'classes dir': 'CLASSES',
+                          'class name': 'MACHINES',
+                          'wwwdir': 'www',
+                          'host columnname': 'Hostname',
+                          'default columns': 'Hostname,Uptime,Status'},
+            'send file': {'send limit': '1'}
+        }
+
+        self.config = configparser()
+
+        # read the config file
+        try:
+            self.config.read('sdi.conf')
+        except:
+            print 'error: bad config file'
+            sys.exit(1)
+
+    def printvars(self, sections):
+        if sections[0] == 'all':
+            sections = self.config.sections()
+
+        for sec in sections:
+            if self.defaults.has_key(sec):
+                for var,value in self.defaults[sec].items():
+                    if not var in [i[0] for i in self.config.items(sec,1)]:
+                        self.config.set(sec,var,value)
+
+            for var,value in self.config.items(sec):
+                print '%s=%s ' %(var.upper().replace(' ',''),value),
+
+    def get(self, section, var):
+        return self.config.get(section, var)
+
+if __name__ == '__main__':
+    if len(sys.argv)==1:
+        sys.exit(1)
+
+    parse = configsdiparser()
+    parse.printvars(sys.argv[1:])
