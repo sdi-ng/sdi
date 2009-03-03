0cd3c37253d1f6be7b115e445dd69f89d73e10a2
Vinicius Ruoso <vkr07@c3sl.ufpr.br>
2009-03-03 10:55:00 -0300

    Moved web pages generation process to sdiweb
    When launching SDI the class specific web files are created just before
    launch all connections to this class. What happens is that some tunnels
    could have already launched, decreasing the performance of web files
    creation.
    
    Moving all the creation proccess before all tunnels launching will decrease
    the launching time. This is a concept change too.
    
    Some tests with a empty machine on 74 hosts showed a little better
    performance, but we expect that ona loaded machine with more hosts the speed
    will be significantly improved.
    
    Signed-off-by: Vinicius Ruoso <vkr07@c3sl.ufpr.br>

--- launchsdi.sh
+++ launchsdi.sh
@@ -18,111 +18,6 @@ test -x "$(which realpath)" ||
 # define STATEDIR
 STATEDIR=$WWWDIR/states
 
-function create_links()
-{
-    FOLDERS[0]="html"
-    FOLDERS[1]="javascript"
-    FOLDERS[2]="css"
-    FOLDERS[3]="img"
-    FOLDERS[4]="langs"
-
-    for FOLDER in ${FOLDERS[@]}; do
-        ln -fs $(realpath $SDIWEB)/$FOLDER $1/ 2> /dev/null
-    done
-
-    if test "$2" = "class"; then
-        ln -fs ../hosts $1/ 2> /dev/null
-    fi
-}
-
-function createclassstructure()
-{
-    CLASS=$1
-
-    SDIMKDIR $WWWDIR/$CLASS || exit 1
-    create_links $WWWDIR/$CLASS class
-}
-
-function createdatastructure()
-{
-    local HOST=$1
-    DATAPATH=$WWWDIR/hosts/$HOST/
-    SDIMKDIR $DATAPATH || exit 1
-    for FILE in $(ls $HOOKS/*/*); do
-        FIELD=$(basename $(realpath $FILE))
-        if ! test -f $DATAPATH/$FIELD.xml; then
-            echo "<$FIELD value=\"\" />" > $DATAPATH/$FIELD.xml
-        fi
-    done
-}
-
-# Create the structure of files that will be used to manage
-# the states of the remote hosts
-function createstatestructure()
-{
-    for state in $SHOOKS/*; do
-        STABLE="true"
-        NAME=$(basename $state)
-        if ! source $state || ! ${NAME}_getstateinfo &> /dev/null; then
-            LOG "ERROR: failure to load state $state"
-            return 1
-        elif test -z "$SSUMARY"; then
-            LOG "ERROR: state $state: \$SUMARY must be set in $state"
-        elif test -z "$SDEFCOLUMNS"; then
-            LOG "ERROR: state $state: \$SDEFCOLUMNS must be set in $state"
-        elif test -z "$STITLE"; then
-            LOG "ERROR: state $state: \$STITLE must be set in $state"
-        else
-            state=$(basename $state)
-            test -f "$STATEDIR/$state.xml" && continue
-            cat <<EOF > $STATEDIR/$state.xml
-<table title="$STITLE" columns="$SDEFCOLUMNS" showtable="$STABLE">
-    <!--#include virtual="../hosts/columns.xml"-->
-    <!--NEW-->
-</table>
-EOF
-            printf "0\n" > "$STATEDIR/$state-count.txt"
-            printf "<$state>$SSUMARY</$state>\n" 0 >\
-            "$STATEDIR/$state-status.xml"
-        fi
-    done
-}
-
-function getcolumns()
-{
-    COLUMNS=""
-    FIELDS=""
-    COMMANDS=$(\ls $PREFIX/commands-enabled/*/*)
-    TEMP=$(mktemp -p $TMPDIR)
-
-    i=0
-    for FIELD in $COMMANDS; do
-        echo "$(basename $FIELD):$FIELD"
-        ((i++))
-    done > $TEMP
-
-    COMMANDS=$(cat $TEMP | cut -d":" -f1 | sort | uniq)
-    for FIELD in $COMMANDS; do
-        FIELDS="$FIELDS $(grep "$FIELD" $TEMP | cut -d":" -f2 | head -1)"
-    done
-
-    for FIELD in $FIELDS; do
-        source $(realpath $FIELD).po
-        getcolumninfo
-
-        # add column to list
-        if test $WEBINTERFACE = true; then
-            FIELD=$(basename $(realpath $FIELD))
-            COLNAME=$(tr ' ' '_' <<< $COLNAME)
-            COLUMNS="$COLUMNS $FIELD:$COLNAME"
-        fi
-        unset WEBINTERFACE COLNAME
-    done
-
-    printf "$COLUMNS"
-    rm $TEMP
-}
-
 # Create necessary folders
 SDIMKDIR $TMPDIR || exit 1
 SDIMKDIR $PIDDIR || exit 1
@@ -132,23 +27,18 @@ SDIMKDIR $STATEDIR || exit 1
 SDIMKDIR $FIFODIR || exit 1
 SDIMKDIR $CLASSESDIR || exit 1
 
+# Start runing tunnels for hosts
+CLASSES=$(ls $CLASSESDIR)
+CLASSESNUM=$(ls $CLASSESDIR |wc -l)
+if test $CLASSESNUM -eq 0; then
+    printf "ERROR: no class set. At least one class of hosts must be defined
+    in $CLASSESDIR directory.\n"
+    exit 1
+fi
+
 # Check if web mode is enabled
 if test $WEBMODE = true; then
-    source $SDIWEB/generatesdibar.sh
-    source $SDIWEB/generateclasspage.sh
-    source $SDIWEB/generatesummary.sh
-    source $SDIWEB/generatexmls.sh
-
-    SDIMKDIR $WWWDIR || exit 1
-    create_links $WWWDIR
-
-    SDIBAR=$(generatesdibar)
-    COLUMNS=$(getcolumns)
-
-    # Create strucure of xml files for states managing
-    printf "Creating states files... "
-    createstatestructure
-    printf "done\n"
+    source $SDIWEB/generatewebfiles.sh
 
     # Start states daemon
     printf "Launching states daemon... "
@@ -161,15 +51,6 @@ fi
 # Open socketdaemon
 $PREFIX/socketdaemon.py & disown
 
-# Start runing tunnels for hosts
-CLASSES=$(ls $CLASSESDIR)
-CLASSESNUM=$(ls $CLASSESDIR |wc -l)
-if test $CLASSESNUM -eq 0; then
-    printf "ERROR: no class set. At least one class of hosts must be defined
-    in $CLASSESDIR directory.\n"
-    exit 1
-fi
-
 # Start sendfile deamon
 DAEMON="$PIDDIRSYS/deamon.pid"
 printf "Launching sendfile deamon... "
@@ -177,40 +58,18 @@ printf "Launching sendfile deamon... "
 (! test -f $DAEMON )) && bash $PREFIX/launchsendfile.sh
 printf "done\n"
 
+# Start launching the tunnels
 COUNT=0
 for CLASS in $CLASSES; do
     ((COUNT++))
-
     printf "Starting $CLASS ($COUNT/$CLASSESNUM)...\n"
     sleep 0.5
 
     HOSTS=$(awk '{print $1}' $CLASSESDIR/$CLASS)
 
-    # Generate sdiweb files
-    if test $WEBMODE = true; then
-        printf "\tCreating web files... "
-        createclassstructure $CLASS
-        generateclasspage $CLASS
-        generatexmls $CLASS "$HOSTS"
-        for HOST in $HOSTS; do
-            createdatastructure $HOST
-        done
-        printf "done\n"
-    fi
-
     # Launch the tunnels
     DAEMON=true bash launchsditunnel.sh "$HOSTS"
-
     sleep 0.5
 done
-
-# Generate summaries
-if test $WEBMODE = true; then
-    printf "Generating summaries... "
-    for SUMMARY in $(\ls $PREFIX/summaries-enabled/* 2> /dev/null); do
-        generatesummary $(basename $(realpath $SUMMARY))
-    done
-    printf "done\n"
-fi
 
 printf "All done.\n"
---
+++ sdiweb/generatewebfiles.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+function create_links()
+{
+    FOLDERS[0]="html"
+    FOLDERS[1]="javascript"
+    FOLDERS[2]="css"
+    FOLDERS[3]="img"
+    FOLDERS[4]="langs"
+
+    for FOLDER in ${FOLDERS[@]}; do
+        ln -fs $(realpath $SDIWEB)/$FOLDER $1/ 2> /dev/null
+    done
+
+    if test "$2" = "class"; then
+        ln -fs ../hosts $1/ 2> /dev/null
+    fi
+}
+
+function createclassstructure()
+{
+    CLASS=$1
+
+    SDIMKDIR $WWWDIR/$CLASS || exit 1
+    create_links $WWWDIR/$CLASS class
+}
+
+function createdatastructure()
+{
+    local HOST=$1
+    DATAPATH=$WWWDIR/hosts/$HOST/
+    SDIMKDIR $DATAPATH || exit 1
+    for FILE in $(ls $HOOKS/*/*); do
+        FIELD=$(basename $(realpath $FILE))
+        if ! test -f $DATAPATH/$FIELD.xml; then
+            echo "<$FIELD value=\"\" />" > $DATAPATH/$FIELD.xml
+        fi
+    done
+}
+
+# Create the structure of files that will be used to manage
+# the states of the remote hosts
+function createstatestructure()
+{
+    for state in $SHOOKS/*; do
+        STABLE="true"
+        NAME=$(basename $state)
+        if ! source $state || ! ${NAME}_getstateinfo &> /dev/null; then
+            LOG "ERROR: failure to load state $state"
+            return 1
+        elif test -z "$SSUMARY"; then
+            LOG "ERROR: state $state: \$SUMARY must be set in $state"
+        elif test -z "$SDEFCOLUMNS"; then
+            LOG "ERROR: state $state: \$SDEFCOLUMNS must be set in $state"
+        elif test -z "$STITLE"; then
+            LOG "ERROR: state $state: \$STITLE must be set in $state"
+        else
+            state=$(basename $state)
+            test -f "$STATEDIR/$state.xml" && continue
+            cat <<EOF > $STATEDIR/$state.xml
+<table title="$STITLE" columns="$SDEFCOLUMNS" showtable="$STABLE">
+    <!--#include virtual="../hosts/columns.xml"-->
+    <!--NEW-->
+</table>
+EOF
+            printf "0\n" > "$STATEDIR/$state-count.txt"
+            printf "<$state>$SSUMARY</$state>\n" 0 >\
+            "$STATEDIR/$state-status.xml"
+        fi
+    done
+}
+
+function getcolumns()
+{
+    COLUMNS=""
+    FIELDS=""
+    COMMANDS=$(\ls $PREFIX/commands-enabled/*/*)
+    TEMP=$(mktemp -p $TMPDIR)
+
+    i=0
+    for FIELD in $COMMANDS; do
+        echo "$(basename $FIELD):$FIELD"
+        ((i++))
+    done > $TEMP
+
+    COMMANDS=$(cat $TEMP | cut -d":" -f1 | sort | uniq)
+    for FIELD in $COMMANDS; do
+        FIELDS="$FIELDS $(grep "$FIELD" $TEMP | cut -d":" -f2 | head -1)"
+    done
+
+    for FIELD in $FIELDS; do
+        source $(realpath $FIELD).po
+        getcolumninfo
+
+        # add column to list
+        if test $WEBINTERFACE = true; then
+            FIELD=$(basename $(realpath $FIELD))
+            COLNAME=$(tr ' ' '_' <<< $COLNAME)
+            COLUMNS="$COLUMNS $FIELD:$COLNAME"
+        fi
+        unset WEBINTERFACE COLNAME
+    done
+
+    printf "$COLUMNS"
+    rm $TEMP
+}
+
+source $SDIWEB/generatesdibar.sh
+source $SDIWEB/generateclasspage.sh
+source $SDIWEB/generatesummary.sh
+source $SDIWEB/generatexmls.sh
+
+create_links $WWWDIR
+
+SDIBAR=$(generatesdibar)
+COLUMNS=$(getcolumns)
+
+# Create strucure of xml files for states managing
+printf "Creating states files... "
+createstatestructure
+printf "done\n"
+
+# Create all files
+printf "Creating web files... "
+for CLASS in $CLASSES; do
+    HOSTS=$(awk '{print $1}' $CLASSESDIR/$CLASS)
+
+    createclassstructure $CLASS
+    generateclasspage $CLASS
+    generatexmls $CLASS "$HOSTS"
+    for HOST in $HOSTS; do
+        createdatastructure $HOST
+    done
+done
+printf "done\n"
+
+# Generate summaries
+printf "Generating summaries... "
+for SUMMARY in $(\ls $PREFIX/summaries-enabled/* 2> /dev/null); do
+    generatesummary $(basename $(realpath $SUMMARY))
+done
+printf "done\n"
