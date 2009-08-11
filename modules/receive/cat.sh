92bbe4351363b1f18f4b4e2b8060104377467ff4
Ricardo Tavares de Oliveira <rto07@c3sl.ufpr.br>
2009-08-11 14:21:34 -0300

    modules/*: Added default modules to send and receive information
    Instead using always a ssh tunnel, we are letting the user
    to choose or write a program able to send and receive information.
    We are adding default modules to do this. Those modules
    do exactly the same ssh tunnel we were using until now.
    
    Signed-off-by: Ricardo Tavares de Oliveira <rto07@c3sl.ufpr.br>
    Signed-off-by: Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>

---
+++ modules/receive/cat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This program will receive the standard output of the sender
+# through the standard input. The standard output of this program
+# will be send to the parser via pipeline.
+
+# This program does nothing but sending its stdin to its stdout.
+
+cat -
