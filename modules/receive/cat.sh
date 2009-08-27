366738eff20e1c8d982f90dcb1d3d10eb7f06d95
Ricardo Tavares de Oliveira <rto07@c3sl.ufpr.br>
2009-08-27 09:14:06 -0300

    ssh.sh, cat.sh: Changed default modules into functions
    We changed the default send and receive modules into functions,
    since they will be sourced my lauchsditunnel.sh, avoiding
    creating some others bash forks.
    
    Signed-off-by: Ricardo Tavares de Oliveira <rto07@c3sl.ufpr.br>
    Signed-off-by: Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>

--- modules/receive/cat.sh
+++ modules/receive/cat.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
-
 # This program will receive the standard output of the sender
 # through the standard input. The standard output of this program
 # will be send to the parser via pipeline.
 
 # This program does nothing but sending its stdin to its stdout.
 
-cat -
+sdireceive()
+{
+    cat -
+}
