de87aced615c559caaed9d5de27b76fa9014aea6
Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>
2010-02-23 22:43:09 -0300

    sdicore.cpp: Removed code of classes Producer/Consumer/Parser/HostMessage
    All this classes have earned their own files
    
    Signed-off-by: Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>

--- src/sdicore.cpp
+++ src/sdicore.cpp
@@ -8,115 +8,19 @@
 #include "sdicore.h"
 #include "socket.h"
 #include "common.h"
+#include "producer.h"
+#include "consumer.h"
+#include "parser.h"
 
 using namespace std;
 
 // Globals
 sem_t* sem_global; // Semaphore
 
-// ------------------------------------------------------------------
-// Implementation of Parser class
-// ------------------------------------------------------------------
-Parser::Parser() {
-    const char* cmd = "parser.sh";
-
-    DEBUG("In parser constructor\n");
-    if ( !(cmdStdin = popen(cmd,"w")) ) {
-        ERROR("Unable to create a Parser: %s\n",strerror(errno));
-        exit(1);
-    }
-}
-
-Parser::~Parser() {
-
-    DEBUG("In parser destructor\n");
-    // Close parser.sh and cmdStdin stream
-    fprintf(cmdStdin,"exit exit exit\nexit exit exit\n",NULL);
-    if ( pclose(cmdStdin) != 0) {
-        WARNING("parser.sh exit with error: %s\n",strerror(errno));
-    }
-}
-
-void Parser::parse(char* host, char* message) {
-
-    DEBUG("Parsing: %s", message);
-    fprintf(cmdStdin,"%s\n",message);
-}
-
-// ------------------------------------------------------------------
-// Implementation of Producer class
-// ------------------------------------------------------------------
-Producer::Producer(list<char*> &messages, sem_t* s) {
-    DEBUG("In Producer constructor\n");
-    msgs = messages;
-    sem = s;
-    SocketServer socket;
-}
-
-Producer::~Producer() {
-    DEBUG("In Producer constructor\n");
-    //TODO: destroy the socket
-}
-
-void Producer::start() {
-
-    sem_wait(sem);
-    msgs.push_front(socket.GetMessage());
-    sem_post(sem);
-}
-
-// ------------------------------------------------------------------
-// Implementation of Consumer class
-// ------------------------------------------------------------------
-Consumer::Consumer(list<char*> &messages, sem_t* s) {
-    DEBUG("In Consumer constructor\n");
-    sem = s;
-    msgs = messages;
-    p = Parser();
-}
-
-void Consumer::consume() {
-    char* message;
-
-    sem_wait(sem);
-    message = msgs.back();
-    msgs.pop_back();
-    sem_post(sem);
-
-    HostMessage hm(message);
-    p.parse(hm.GetHost(),hm.GetMessage());
-
-}
-
-Consumer::~Consumer() {
-    delete &p;
-}
-
-// ------------------------------------------------------------------
-// Implementation of HostMessage class
-// ------------------------------------------------------------------
-HostMessage::HostMessage(char* msg) {
-    char* idxmsg = strchr(msg, ' ');
-    int n = idxmsg - msg;
-    int nend = strlen(msg)-n;
-
-    host = (char*) malloc( n*sizeof(char)+1 );
-    message = (char*) malloc( nend*sizeof(char)+1 );
-
-    strncpy(host,msg,n);
-    strncpy(message,&msg[n+1],nend);
-    host[n] = '\0';
-    message[strlen(msg)] = '\0';
-}
-
-HostMessage::~HostMessage() {
-    free(host);
-    free(message);
-}
-
 // This thread is reponsible to keep the consumer consuming
 void* consumer_thread(void* threadarg) {
-    consumer_thread_t* ct = (consumer_thread_t*) threadarg;
+    consumer_thread_t* ct;
+    ct = (consumer_thread_t*) malloc(sizeof(consumer_thread_t));
     Consumer c(*ct->messages,sem_global);
     while ( !ct->quit ) {
         c.consume();
--- src/sdicore.h
+++ src/sdicore.h
@@ -2,58 +2,11 @@
 #define SDI_CORE_H
 
 #include <list>
-#include <semaphore.h>
-#include "socket.h"
+#include "consumer.h"
 
 using namespace std;
 
 #define MAXCONSUMER 100;
-
-// Separates msg into two elements: host and message
-class HostMessage {
-    public:
-        HostMessage(char* msg);
-        ~HostMessage();
-        char* GetHost() const { return host; };
-        char* GetMessage() const { return message; };
-    private:
-        char* host;
-        char* message;
-};
-
-// Parser the messages returned by hosts
-class Parser {
-    public:
-        Parser();
-        ~Parser();
-        void parse(char* host, char* message);
-    private:
-        FILE* cmdStdin;
-};
-
-// Read socket's and insert messages into a list
-class Producer {
-    public:
-        Producer(list<char*> &messages, sem_t* s);
-        ~Producer();
-        void start();
-    private:
-        list<char*> msgs;
-        SocketServer socket;
-        sem_t* sem;
-};
-
-// Read messages from a list
-class Consumer {
-    public:
-        Consumer(list<char*> &messages, sem_t* s);
-        ~Consumer();
-        void consume();
-    private:
-        list<char*> msgs;
-        sem_t* sem;
-        Parser p;
-};
 
 // Defining some useful types
 
