d94bbd24b7f27139f15a0ec7e1810a5e2554e916
Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>
2010-02-23 22:33:44 -0300

    Created consumer.{cpp,h} to store Consumer class
    This class is responsable to pick up a message from a list and send it to a
    instance of Parser class
    
    Signed-off-by: Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>

---
+++ src/consumer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "common.h"
+#include "hostmessage.h"
+#include "consumer.h"
+
+Consumer::Consumer(list<char*> &messages, sem_t* s) {
+    DEBUG("In Consumer constructor\n");
+    sem = s;
+    msgs = messages;
+    p = Parser();
+}
+
+void Consumer::consume() {
+    char* message;
+
+    sem_wait(sem);
+    message = msgs.back();
+    msgs.pop_back();
+    sem_post(sem);
+
+    HostMessage hm(message);
+    p.parse(hm.GetHost(),hm.GetMessage());
+
+}
+
+Consumer::~Consumer() {
+    delete &p;
+}
---
+++ src/consumer.h
@@ -0,0 +1,23 @@
+#ifndef __CONSUMER_H_
+#define __CONSUMER_H_
+
+#include <iostream>
+#include <list>
+#include <semaphore.h>
+#include "parser.h"
+
+using namespace std;
+
+// Read messages from a list
+class Consumer {
+    public:
+        Consumer(list<char*> &messages, sem_t* s);
+        ~Consumer();
+        void consume();
+    private:
+        list<char*> msgs;
+        sem_t* sem;
+        Parser p;
+};
+
+#endif
