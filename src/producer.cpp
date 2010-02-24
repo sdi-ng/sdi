90cd53e9fb444c52e552d85aaed311cd600ba408
Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>
2010-02-23 22:35:51 -0300

    Created producer.{cpp,h} to store Producer class
    This class is responsable for pick up a message from a socket and then
    insert it into a list
    
    Signed-off-by: Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>

---
+++ src/producer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+#include <semaphore.h>
+#include "producer.h"
+#include "common.h"
+#include "hostmessage.h"
+
+Producer::Producer(list<char*> &messages, sem_t* s) {
+    DEBUG("In Producer constructor\n");
+    msgs = messages;
+    sem = s;
+    SocketServer socket;
+}
+
+Producer::~Producer() {
+    DEBUG("In Producer destructor\n");
+    //TODO: destroy the socket
+}
+
+void Producer::start() {
+
+    sem_wait(sem);
+    msgs.push_front(socket.GetMessage());
+    sem_post(sem);
+}
---
+++ src/producer.h
@@ -0,0 +1,20 @@
+#ifndef __PRODUCER_H_
+#define __PRODUCER_H_
+
+#include <list>
+#include <semaphore.h>
+#include "socket.h"
+
+// Read socket's and insert messages into a list
+class Producer {
+    public:
+        Producer(list<char*> &messages, sem_t* s);
+        ~Producer();
+        void start();
+    private:
+        list<char*> msgs;
+        SocketServer socket;
+        sem_t* sem;
+};
+
+#endif
