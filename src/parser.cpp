41c63a895ad133fbc1321cf2e02c6c1cf6b48d52
Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>
2010-03-01 16:52:32 -0300

    parser.{cpp,h}: Created a thread function to periodically flush cmdStdin
    This flush isn't done at every write because implies in a big overhead. So,
    every instance of Parser now manages a thread which do a periodic flush in
    cmdStdin.
    
    Signed-off-by: Diego Giovane Pasqualin <dgp06@c3sl.ufpr.br>
    Acked-by: Ricardo T Oliveira <rto07@c3sl.ufpr.br>
    Acked-by: Vinicius Ruoso <vkr07@c3sl.ufpr.br>
    Acked-by: Bruno Ribas <ribas@c3sl.ufpr.br>

--- src/parser.cpp
+++ src/parser.cpp
@@ -6,6 +6,17 @@
 #include "common.h"
 #include "parser.h"
 
+void* parser_flush(void* ta) {
+    DEBUG("p Entering parser_flush\n");
+    p_flush_t *targ = (p_flush_t*) ta;
+    while (!targ->quit) {
+        DEBUG("p flushing cmdStdin\n");
+        fflush(targ->cmdStdin);
+        sleep(FLUSHPERIOD);
+    }
+    DEBUG("p Ending parser_flush\n");
+}
+
 Parser::Parser() {
     const char* cmd = "./parser.sh";
 
@@ -14,6 +25,9 @@ Parser::Parser() {
         ERROR("Unable to create a Parser: %s\n",strerror(errno));
         exit(1);
     }
+    thread.quit = false;
+    thread.cmdStdin = cmdStdin;
+    pthread_create(&(thread.tid),NULL,parser_flush,(void*)&thread);
 }
 
 Parser::~Parser() {
@@ -23,6 +37,8 @@ Parser::~Parser() {
     if ( pclose(cmdStdin) != 0) {
         WARNING("parser.sh exit with error: %s\n",strerror(errno));
     }
+    thread.quit = true;
+    pthread_join(thread.tid,NULL);
 }
 
 void Parser::parse(char* host, char* message) {
--- src/parser.h
+++ src/parser.h
@@ -3,6 +3,14 @@
 
 #include <stdio.h>
 
+#define FLUSHPERIOD 30
+
+typedef struct p_flush {
+        FILE* cmdStdin;
+        pthread_t tid;
+        bool quit;
+} p_flush_t;
+
 // Parser the messages returned by hosts
 class Parser {
     public:
@@ -11,6 +19,7 @@ class Parser {
         void parse(char* host, char* message);
     private:
         FILE* cmdStdin;
+        p_flush_t thread;
 };
 
 #endif
